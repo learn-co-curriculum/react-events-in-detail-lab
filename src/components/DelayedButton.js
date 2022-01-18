c1a0f39ac411eaf104cf1b16bfc61b865cbd66c0
Milan <milanm1120@gmail.com>
2022-01-17 20:55:24 -0500

    commit tree 41c81680e0166a115948221013b9e5d5506d7b40

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
 // Code DelayedButton Component Here
+
+import React from 'react';
+
+class DelayedButton extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default DelayedButton
